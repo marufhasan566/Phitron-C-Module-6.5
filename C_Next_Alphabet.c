44a137a2b98387f9639608808a7d96b43676f2da
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-12 16:43:05 +0600

    all code fixed

--- C_Next_Alphabet.c
+++ C_Next_Alphabet.c
@@ -3,7 +3,15 @@ int main()
 {
     char ch;
     scanf("%c",&ch);
-    char CH= ch+1;
-    printf("%c\n",CH);
+    // printf("%d\n",ch);
+    if(ch==122){
+        char CH= ch-25;
+        printf("%c\n",CH);
+    }
+    else{
+        char CH= ch+1;
+        printf("%c\n",CH);
+    }
+    // printf("%d\n",CH);
     return 0;
 }
--- I_Lucky_Numbers.c
+++ I_Lucky_Numbers.c
@@ -6,11 +6,24 @@ int main()
     int first = a/10;
     int second = a%10;
     // printf("%d %d\n",first,second);  
-    if(first%second==0 || second%first==0){
-        printf("YES");
-    }  
-    else{
-        printf("NO");
+    if(second!=0){
+        if(first%second==0 || second%first==0){
+            printf("YES");
+        }
+        else{
+            printf("NO");
+        } 
     }
+    else if(second==0){
+        if(second%first==0){
+            printf("YES");
+        } 
+        else{
+            printf("NO");
+        }
+    }
+    // else{
+    //     printf("NO");
+    // }
     return 0;
 }
