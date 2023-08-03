3fc6a0ce888a51bb0b700a590d985b9b62d639b4
marufhasan566 <marufhasan.mh1@gmail.com>
2023-08-03 23:37:48 +0600

    Refreshed

--- I_Lucky_Numbers.c
+++ I_Lucky_Numbers.c
@@ -1,26 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int a;
-    scanf("%d",&a);
-    int first = a/10;
-    int second = a%10;
-    // printf("%d %d\n",first,second);  
-    if(second!=0){
-        if(first%second==0 || second%first==0){
+    scanf("%d", &a);
+    int first = a / 10;
+    int second = a % 10;
+    // printf("%d %d\n",first,second);
+    if (second != 0)
+    {
+        if (first % second == 0 || second % first == 0)
+        {
             printf("YES");
         }
-        else{
+        else
+        {
             printf("NO");
-        } 
+        }
     }
-    else if(second==0){
-        if(second%first==0){
-            printf("YES");
-        } 
-        else{
-            printf("NO");
-        }
+    else if (second == 0)
+    {
+        printf("YES");
+        // if (second % first == 0)
+        // {
+        //     printf("YES");
+        // }
+        // else{
+        //     printf("NO");
+        // }
     }
     // else{
     //     printf("NO");
